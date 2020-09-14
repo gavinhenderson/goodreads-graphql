5325df8af78556a9db48d3a98e990848209f725d
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2020-09-14 16:13:01 +0100

    Create a hello world graphql example

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const { ApolloServer, gql } = require("apollo-server-express");
 
 const app = express();
 
@@ -12,5 +13,22 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "../app/build")));
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!",
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app });
 
 module.exports = app;
