0054e0b46df32324ac691fc764ec0271930337ae
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2020-09-14 16:15:47 +0100

    Put schema graphql in its own folder

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const { ApolloServer, gql } = require("apollo-server-express");
+const server = require("./graphql/server");
 
 const app = express();
 
@@ -14,21 +14,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "../app/build")));
 
-// Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
-// Provide resolver functions for your schema fields
-const resolvers = {
-  Query: {
-    hello: () => "Hello world!",
-  },
-};
-
-const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app });
 
 module.exports = app;
