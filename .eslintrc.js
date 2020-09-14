4592ad49f6276f136e8d34aa48da3e1198b81f49
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2020-09-14 16:32:20 +0100

    Sort eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["standard"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    quotes: ["error", "double"],
+    "comma-dangle": ["error", "only-multiline"],
+    semi: ["error", "always"],
+    "space-before-function-paren": "off",
+  },
+};
