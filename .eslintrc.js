325f3abe729f1c8995a0fbf54ed89b9bfdfff743
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2020-09-15 16:40:36 +0100

    Fix eslint i think

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,9 @@
 module.exports = {
   env: {
-    browser: true,
-    es2021: true,
+    node: true,
   },
   extends: ["standard"],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: "module",
-  },
+
   rules: {
     quotes: ["error", "double"],
     "comma-dangle": ["error", "only-multiline"],
